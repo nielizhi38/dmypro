873f71b92e5f236972bb5a608549f308f7f016db
nielz <nielz@163.com>
2016-04-17 11:35:17 +0800

    modify master

--- hello.java
+++ hello.java
@@ -1,12 +1,5 @@
-"public class Hello { }" 
-"function test2(){}" 
-"没有把文件暂存" 
-"hhhhh"
-"world .good"
-yootk
-上面add 还没有commit
-<<<<<<< HEAD
- in new brh branch"hello new year" 
-=======
-.COM"wo shi brh "
->>>>>>> brh
+public class Hello{
+	public print(){
+		System.out.print("hello world");
+	}
+}
